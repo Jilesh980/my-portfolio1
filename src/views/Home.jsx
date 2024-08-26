43858991bf401617487529e1b7fcb2b2e7f8de58
Jilesh980 <88798168+Jilesh980@users.noreply.github.com>
2024-08-26 19:28:28 -0400

    all comit

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -66,9 +66,25 @@ const Home = () => {
             </div>
             <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
               <div className="mt-3 sm:mt-0 cursor-pointer w-1/2">
-                <Link className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-blue-500 hover:bg-blue-200 md:py-4 md:text-lg md:px-10">
-                  Resume
-                </Link>
+              <a
+  href="https://drive.google.com/file/d/1UayLAnTkwrwM3H8KktMMGyhL-hP0EJT_/view?usp=sharing"
+  target="_blank"
+  style={{
+    display: "inline-block",
+    backgroundColor: "#007bff",
+    color: "white",
+    padding: "12px 24px",
+    fontSize: "16px",
+    fontWeight: "bold",
+    textDecoration: "none",
+    borderRadius: "8px",
+    transition: "all 0.3s ease",
+    cursor: "pointer",
+  }}
+>
+  Resume
+</a>
+
               </div>
             </div>
           </div>
--- src/views/Services.jsx
+++ src/views/Services.jsx
@@ -1,51 +1,59 @@
 import React, { useContext } from "react";
-import Card from "../components/card";
+import { serviceData } from "../constants";
 import { ThemeContext } from "../themeProvider";
+import { motion } from "framer-motion";
 
-const Projects = () => {
+const Services = () => {
   const theme = useContext(ThemeContext);
-  const darkMode = theme.state.darkMode;
-
   return (
     <div
-      id="projects"
-      className={darkMode ? "bg-white text-black" : "bg-gray-900 text-white"}
+      className={
+        theme.state.darkMode ? "pb-20 bg-fixed bg-gray-100" : "pb-20 bg-black"
+      }
+      // style={{backgroundImage: `url('https://i.pinimg.com/originals/b0/b1/f5/b0b1f5d33de00e3c21ad29bbba25e31b.gif')`}}>
     >
-      <div className="max-w-7xl mx-auto x-4 sm:px-6 lg:px-8 px-4  pt-24 pb-12">
-        <h2 className="text-5xl font-bold px-4 md:px-0 text-center">
-          My SKills
-        </h2>
-        <h4 className="mt-16 text-3xl font-semibold text-blue-600">
-          What I Can do
-        </h4>
-        <div className="mt-8 flex justify-between items-stretch flex-wrap">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </div>
-        <a
-          href="hello"
-          class=" w-32 flex items-center py-4 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mx-auto mt-4"
+      <div
+        className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 px-4 pt-20"
+        id="services"
+      >
+        <h2
+          className={
+            theme.state.darkMode
+              ? "text-5xl font-bold px-4 md:px-0 text-center"
+              : "text-5xl font-bold px-4 md:px-0 text-center text-white"
+          }
         >
-          Show More
-          <svg
-            class="ml-2 -mr-1 w-4 h-4"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </a>
+          Services
+        </h2>
+        <div className="">
+          <h4 className="mt-16 text-3xl font-semibold text-blue-500">
+            What I Provide
+          </h4>
+          <div className="mt-8 flex md:flex-row justify-between flex-col md:items-stretch items-center ">
+            {serviceData.map((el) => (
+              <motion.div
+                initial="hidden"
+                whileInView={"visible"}
+                variants={{
+                  visible: { opacity: 1, scale: 1 },
+                  hidden: { opacity: 0, scale: 0 },
+                }}
+                className={
+                  theme.state.darkMode
+                    ? "md:w-96 p-4 bg-white rounded-lg flex items-center flex-col mt-8"
+                    : "md:w-96 p-4 bg-gray-100 rounded-lg flex items-center flex-col mt-8"
+                }
+              >
+                <img src={el.img} alt="" />
+                <h4 className="text-xl font-bold mt-4">{el.name}</h4>
+                <p className="text-lg mt-2 text-justify">{el.desc}</p>
+              </motion.div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Projects;
+export default Services;
